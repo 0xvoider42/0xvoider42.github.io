9eaabd8fee72ae88003931daa56c688890df701c
Nikoloz <nick@cryptobuilders.io>
2021-11-09 11:23:57 +0200

    adds footer

---
+++ software-project/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <p className='footer-generic'>Welcome to the world of crypto @</p>
+    </div>
+  );
+}
+
+export default Footer;
--- software-project/src/components/pages/Home.js
+++ software-project/src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import '../../App.css';
 import React from 'react';
 import Hero from '../Hero.js';
+import Footer from '../Footer.js';
 
 function Home() {
   return (
     <>
       <Hero />
+      <Footer />
     </>
   );
 }
